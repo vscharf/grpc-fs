e8e163febd97c275a50b6d28e3eb804ab8e45915
Veit Scharf <veit.scharf@kip.uni-heidelberg.de>
2015-03-31 11:18:34 +0200

    tranfer with nc for single request works

--- src/server/grpcfs_server.cc
+++ src/server/grpcfs_server.cc
@@ -1,6 +1,9 @@
+#include <fstream>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
+#include <mutex>
 
 #include <grpc/grpc.h>
 #include <grpc++/server.h>
@@ -11,47 +14,136 @@
 #include <grpc++/stream.h>
 #include "grpc_fs.pb.h"
 
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::ServerWriter;
 using grpc::Status;
 using grpcfs::FileRequest;
-using grpcfs::FileChunk;
+using grpcfs::FileRequestAnswer;
 using grpcfs::FileServer;
 
+template<std::streamsize BLOCK_SIZE>
 class FileServerImpl final : public FileServer::Service {
 public:
-  Status GetFile(ServerContext* context, const FileRequest* request,
-                 ServerWriter<FileChunk>* writer) override
+  Status GetFile(ServerContext* context, const ::grpcfs::FileRequest* request, ::grpcfs::FileRequestAnswer* response) override
   {
-    std::cout << "Client requested file: " << request->name() << std::endl;
-    FileChunk fc;
-    fc.set_chunk("some chunk");
-    writer->Write(fc);
+    std::string n = request->name();
+    std::string h = request->host();
+    int p = request->port();
+
+    {
+      std::lock_guard<std::mutex> lg(cout_mutex_);
+      std::cerr << "Request for file " << n << " from " << h << " to port " << p << std::endl;
+      (std::cout << n << " " << h << " " << p << " ").flush();
+      std::cerr << "... submitted" << std::endl;
+    }
+
+    response->set_status(grpcfs::FileRequestAnswer_Status_OK);
+
     return Status::OK;
   }
 private:
-  
+  char P_[BLOCK_SIZE];
+  std::mutex cout_mutex_;
 };
 
+class Spawner
+{
+public:
+  static void Start() {
+    while(1) {
+      std::string n, h;
+      int p;
+      std::cin >> n >> h >> p;
+      if(!std::cin) {
+        std::cerr << "Error reading from STDIN!\n";
+        std::cin >> std::cerr.rdbuf();
+        std::cerr << std::endl;
+        return;
+      }
+
+      std::cerr << "Spawning thread for " << h << ":" << p << " ..." << n << std::endl; 
+      std::thread t(spawn<4096>, n, h, p);
+      t.join();
+      std::cerr << "... done." << std::endl; 
+    }
+  }
+private:
+  template <std::streamsize BLOCK_SIZE>
+  static void spawn(std::string n, std::string h, int p) {
+    char P_[BLOCK_SIZE];
+    FILE* pipe_fp;
+    if((pipe_fp = popen(("nc -v -q 0 " + h + " " + std::to_string(p) + " 1>nc.log 2>nc.err").c_str(), "we")) == NULL) {
+      perror("popen");
+      std::cerr << "popen failed" << std::endl;
+      return;
+    }
+    
+    FILE* F = fopen(n.c_str(), "re");
+    std::size_t N(0);
+    while((N = fread(P_, sizeof(char), BLOCK_SIZE, F)) == BLOCK_SIZE) {
+      fwrite(P_, sizeof(char), BLOCK_SIZE, pipe_fp);
+    }
+    fwrite(P_, sizeof(char), N, pipe_fp);
+    std::cerr << "finished sending ..." << std::endl;
+    pclose(pipe_fp);
+    fclose(F);
+  }
+};
 void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  FileServerImpl service;
+  std::string server_address("192.168.0.247:50051");
+  FileServerImpl<4096> service;
 
   ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
   std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+  std::cerr << "Server listening on " << server_address << std::endl;
   server->Wait();
 }
 
+void RunSpawner() {
+  Spawner::Start();
+}
+
 int main(int argc, char** argv) {
-  grpc_init();
+  // fork and start spawner
+  FILE* F;
 
-  RunServer();
+  int filedes[2];
+  if (pipe(filedes) == -1) {
+    perror("pipe");
+    exit(1);
+  }
 
-  grpc_shutdown();
+  pid_t pid = fork();
+  if (pid == -1) {
+    perror("fork");
+    exit(1);
+  } else if (pid == 0) {
+    close(filedes[1]);
+    while ((dup2(filedes[0], STDIN_FILENO) == -1) && (errno == EINTR)) {}
+    close(filedes[0]);
+
+    RunSpawner();
+
+  } else {
+    close(filedes[0]);
+    while ((dup2(filedes[1], STDOUT_FILENO) == -1) && (errno == EINTR)) {}
+    grpc_init();
+    
+    RunServer();
+    
+    grpc_shutdown();
+  }
+
   return 0;
 }
---
+++ tests/performance-tests-remote/test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+
+
+SCRIPT=`mktemp`
+
+cat <<'EOF' > $SCRIPT
+#/bin/bash -xe
+source /afsuser/vscharf/WorkArea/GrpcFileServer/setup.sh
+echo $files
+for A in $files
+do
+  echo $A
+  /lib64/ld-linux-x86-64.so.2 /afsuser/vscharf/WorkArea/GrpcFileServer/grpc-fs/bins/grpcfs_client_static $A /dev/null
+done
+EOF
+
+BASEDIR=/data/users/vscharf/WyyAnalysis/skims/data
+
+# files
+declare -a FILE_ARR
+FILE_ARR[5]="$BASEDIR/211867.0.root"
+FILE_ARR[1]="$BASEDIR/212721.0.root"
+FILE_ARR[2]="$BASEDIR/209864.0.root"
+FILE_ARR[3]="$BASEDIR/213039.0.root"
+FILE_ARR[4]="$BASEDIR/207982.0.root"
+#FILE_ARR[0]="$BASEIDR/214680.0.root $BASEDIR/212199.0.root"
+#FILE_ARR[1]="$BASEDIR/213951.0.root $BASEDIR/211787.0.root"
+#FILE_ARR[2]="$BASEDIR/215473.0.root $BASEDIR/209580.0.root"
+#FILE_ARR[3]="$BASEDIR/214777.0.root $BASEDIR/209995.0.root"
+#FILE_ARR[4]="$BASEDIR/208781.0.root $BASEDIR/209254.0.root"
+#FILE_ARR[5]="$BASEDIR/209629.0.root $BASEDIR/208485.0.root"
+#FILE_ARR[6]="$BASEDIR/207447.0.root $BASEDIR/203602.0.root"
+#FILE_ARR[7]="$BASEDIR/204763.0.root $BASEDIR/205071.1.root"
+#FILE_ARR[8]="$BASEDIR/204265.0.root $BASEDIR/209736.0.root"
+#FILE_ARR[9]="$BASEDIR/205055.0.root $BASEDIR/212172.0.root"
+
+for I in {1..2}
+do
+  qsub -q Short -l nodes=hen0$I.atlas-farm.kip.uni-heidelberg.de -v files="${FILE_ARR[I]}" $SCRIPT
+#  cat <<EOF | qsub -q Short -l nodes=hen0$I.atlas-farm.kip.uni-heidelberg.de
+#!/bin/bash -e
+#source /afsuser/vscharf/WorkArea/GrpcFileServer/setup.sh
+#echo "Files: ${FILE_ARR[I]}"
+#for F in ${FILE_ARR[I]}; do
+#  echo "Transferring \$F ..."
+#  /lib64/ld-linux-x86-64.so.2 /afsuser/vscharf/WorkArea/GrpcFileServer/grpc-fs/tests/performance-tests-remote/transfer_disk_disk_static \$F /dev/null || exit 1
+#done
+#EOF
+done
